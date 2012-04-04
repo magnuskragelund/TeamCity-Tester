94ae0fea890262a04bc751b8a343820150218f6d
magnuskragelund <magnuskragelund@gmail.com>
2012-04-04 22:31:13 +0200

    Adds Common project, and a cool view.

---
+++ Common/StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Common
+{
+    static public class StringExtensions
+    {
+        public static string AppendDude(this string s)
+        {
+            return s + " Dude!";
+        }
+    }
+}
---
+++ WepApp/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using Common;
+
+namespace WepApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hey, ".AppendDude();
+            return View();
+        }
+
+    }
+}
